1c1f04f58b0d0d3e299aa49c8a02da39ec76141e
Artur Begyan <arturbegyan98@gmail.com>
2024-03-09 19:19:55 +0300

    feat: deployment functions

---
+++ deploy/priceFeedOracleUSDT.deploy.ts
@@ -0,0 +1,27 @@
+import { deployments, getNamedAccounts } from "hardhat";
+import { HardhatRuntimeEnvironment } from "hardhat/types";
+import { DeployFunction } from "hardhat-deploy/types";
+
+const func: DeployFunction = async (hre: HardhatRuntimeEnvironment) => {
+  if (hre.network.name !== "hardhat") {
+    console.log(
+      `Deploying PriceFeedOracleWithAdapter to ${hre.network.name}. Hit ctrl + c to abort`
+    );
+  }
+
+  const { deploy } = deployments;
+  const { deployer } = await getNamedAccounts();
+
+  const adapterUSDT = await deploy("RedstonePriceFeedWithRoundsUSDT", {
+    from: deployer,
+    log: hre.network.name !== "hardhat",
+    proxy: {
+      proxyContract: "EIP173Proxy"
+    }
+  });
+  console.log(`Deployed Price Feed USDT to ${adapterUSDT.address}`);
+};
+
+export default func;
+
+func.tags = ["USDTWR"];
